549565b7ff8fcdea24da87604fe143625f5fb3e1
carolinaolv <carolinaolatevallejos96@gmail.com>
2019-06-03 16:52:04 -0400

    agregando card en la seccion de proyectos

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
 document.getElementById('ability').addEventListener('click', (evento) =>{
     
     document.getElementById('about-me').style.display='none';
+    document.getElementById('projects').style.display='none';
     document.getElementById('skills').style.display='block';
+
+})
+document.getElementById('scheme').addEventListener('click', (evento) =>{
+  
+  document.getElementById('about-me').style.display='none';
+  document.getElementById('skills').style.display='none';
+  document.getElementById('projects').style.display='block';
 
 })
 
