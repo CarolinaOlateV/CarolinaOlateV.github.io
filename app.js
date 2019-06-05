ae6e796df935959aac878e3f613c3f8c603fad67
carolinaolv <carolinaolatevallejos96@gmail.com>
2019-06-05 17:00:43 -0400

    agregando contacto

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ document.getElementById('ability').addEventListener('click', (evento) =>{
     
     document.getElementById('about-me').style.display='none';
     document.getElementById('projects').style.display='none';
+    document.getElementById('contact').style.display='none';
     document.getElementById('skills').style.display='block';
 
 })
@@ -9,7 +10,16 @@ document.getElementById('scheme').addEventListener('click', (evento) =>{
   
   document.getElementById('about-me').style.display='none';
   document.getElementById('skills').style.display='none';
+  document.getElementById('contact').style.display='none'
   document.getElementById('projects').style.display='block';
+
+})
+document.getElementById('contact-me').addEventListener('click', (evento) =>{
+  
+  document.getElementById('about-me').style.display='none';
+  document.getElementById('skills').style.display='none';
+  document.getElementById('projects').style.display='none'
+  document.getElementById('contact').style.display='block';
 
 })
 
