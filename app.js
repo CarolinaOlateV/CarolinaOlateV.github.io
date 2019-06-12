dcbd9bf96b0dbfd766fe2a19a86399f797c1ce01
carolinaolv <carolinaolatevallejos96@gmail.com>
2019-06-12 18:16:33 -0400

    cambiando carousel y agregando cards

--- app.js
+++ app.js
@@ -23,25 +23,3 @@ document.getElementById('contact-me').addEventListener('click', (evento) =>{
 
 })
 
-new Vue({
-    el: '#example',
-    data: {
-      slides: 7
-    },
-    methods: {
-      onAfterSlideChange(index) {
-        console.log('@onAfterSlideChange Callback Triggered', 'Slide Index ' + index)
-      },
-      onBeforeSlideChange(index) {
-        console.log('@onBeforeSlideChange Callback Triggered', 'Slide Index ' + index)
-      },
-      onLastSlide(index) {
-        console.log('@onLastSlide Callback Triggered', 'Slide Index ' + index)
-      }
-    },
-    components: {
-      'carousel-3d': Carousel3d.Carousel3d,
-      'slide': Carousel3d.Slide
-    }
-  })
-  
